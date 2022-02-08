07901722d14761f26f3af1e00c19d1c64be3064b
Marc <marcsicr@gmail.com>
2022-02-08 17:03:07 +0100

    game loading bar

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,11 @@
-import { Box, IconButton, GlobalStyles } from '@mui/material'
+import { useState } from 'react'
+import {
+  Box,
+  IconButton,
+  GlobalStyles,
+  Typography,
+  LinearProgress
+} from '@mui/material'
 import FullscreenIcon from '@mui/icons-material/Fullscreen'
 import { styled } from '@mui/material/styles'
 
@@ -7,21 +14,21 @@ import Unity, { UnityContext } from 'react-unity-webgl'
 import { SectionHeader } from 'components/sections/Section'
 
 const GameBox = styled(Box)(({ theme }) => ({
-
-  [theme.breakpoints.down('md')]:{
+  [theme.breakpoints.down('md')]: {
     width: '100%',
-    height: '100%',
+    height: '100%'
   },
 
   width: '80%',
   height: '80%',
   margin: '30px auto',
-  position: 'relative',
-
+  position: 'relative'
 }))
 
 export default function Game({ path }) {
   const gameFolder = 'games/crossy'
+
+  const [progression, setProgression] = useState(0)
 
   const unityContext = new UnityContext({
     loaderUrl: `${gameFolder}/web.loader.js`,
@@ -30,35 +37,47 @@ export default function Game({ path }) {
     codeUrl: `${gameFolder}/web.wasm`
   })
 
+  unityContext.on('progress', function (progression) {
+    setProgression(progression)
+  })
+
   function handleOnClickFullscreen() {
     unityContext.setFullscreen(true)
   }
 
-  return (<>
-  <GlobalStyles
-  styles={(theme) => ({
-    body: { backgroundColor: theme.palette.background.secondary }
-  })
-  }
-/>
-     
+  return (
+    <>
+      <GlobalStyles
+        styles={(theme) => ({
+          body: { backgroundColor: theme.palette.background.secondary }
+        })}
+      />
+
       <GameBox>
-      <SectionHeader msg={"CROSSY CROAK"}></SectionHeader>
+        <SectionHeader msg={'CROSSY CROAK'}></SectionHeader>
+
+        {progression < 1 ? (
+          <>
+            <Typography>Loading game {progression * 100} %...</Typography>
+            <LinearProgress variant="determinate" value={progression * 100} />
+          </>
+        ) : (
+          <IconButton
+            sx={{ position: 'absolute', right: 0, color: 'white' }}
+            onClick={() => {
+              handleOnClickFullscreen()
+            }}
+          >
+            <FullscreenIcon style={{ fontSize: '36px' }} />
+          </IconButton>
+        )}
+
         <Unity
           onClick={handleOnClickFullscreen}
           unityContext={unityContext}
           style={{ height: '100%', width: '100%' }}
         />
-
-        <IconButton
-          sx={{position:'absolute', right:0, color:'white'}}
-          onClick={() => {
-            handleOnClickFullscreen()
-          }}
-        >
-          <FullscreenIcon style={{fontSize:'36px'}}/>
-        </IconButton>
       </GameBox>
-      </>
+    </>
   )
 }
